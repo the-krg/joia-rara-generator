2ca878629f5f7175ef693da8151474b40ee627c6
Enzo Vinicius <31260493+the-krg@users.noreply.github.com>
2019-05-13 11:11:23 -0300

    Create xablau.rb

---
+++ xablau.rb
@@ -0,0 +1,79 @@
+class Xablau
+  def generate
+    {
+      paula: [
+        "Faça ou não faça, mas nem tente",
+        "Cagou né, tá felizão.",
+        "[edited] Tá felizão né? Cagou...",
+        "O itamar está ali com o frizzo 'frizzando' alguma coisa"
+      ],
+      jok: [
+        "Não contemple o céu do fundo de um poço",
+        "Ohio que te parta",
+        "Acidente Report",
+        "Eu podia estar matando, roubando, sequestrando ou pedindo esmola, mas, estou aqui pedindo a sua aprovação no meu Pull Request…",
+        "Como eu amo JS <3",
+        "Falou em Payout, falou em problema",
+        "Vai ficar debaixo da mesa, chorando baixinho, pra não atrapalhar ninguém.",
+        "Vou te sugerir uma mudança no seu código, mas te digo antes que não tem por que você fazer isso",
+        "git commit -m 'Resolvendo a bagunça'"
+      ],
+      sully: [
+        "marca um 10",
+        "Resolves `a loja da família` bug",
+        "O que mais chove aqui em Curitiba",
+        "CARALHO o meu rack chegou",
+        "Para que tenha garantia de futuras mudanças com garantias que manteremos o sistema estável.",
+        "Não no demonio que ele aparece",
+        "É tipo um biscoito chinês, só que parrudo",
+        "if key == 'succxaess'",
+        "você tem o token no seu cookie",
+        "Mal'criado.. Mal'daner"
+      ],
+      enzo: [
+        "Se eu pudesse voltar no tempo, eu comeria um pouquinho menos todos os dias só pra comer mais hoje.",
+        "Eu nunca maltratei ninguém que não seja eu mesmo",
+        "Dá seus polling",
+        "LTTM - Looks TOP to me",
+        "O fato de que você Não sabe que você Não sabe, significa que você sabe de algo",
+        "\#{Faker::Dessert.variety} without fear",
+        "Deploy without fear",
+        "O segredo pro teste sempre passar? Nunca falhe."
+      ],
+      ita: [
+        "Mais Real com a Realidade",
+        "Alerta de Long Polling",
+        "Eu nunca durmo no mesmo dia que eu acordo",
+        "Itamar BBQ em casa"
+      ],
+      guevara: [
+        "xhurrasquey ou xhurrascou ?"
+      ],
+      maldaner: [
+        "Se fosse mais específico, não seria tão genérico"
+      ],
+      ju: [
+        "ShedRift"
+      ],
+      nass: [
+        "if(if) {\n
+           urra();\n
+        }",
+        "Patrick Esponja"
+      ],
+      anon: [
+        "function toggleStuff() { ...",
+        "Solicitação de pagamento Ebanx. Pedido 40028922.",
+        "pay without fear",
+        "Precisamos de teste de teste no comportamento",
+        "Nada mais infinito que uma gambiarra temporária",
+        "Temos algum problema geral com o Dashboard? algo que possa estar causando falta de dados"
+      ]
+    }
+  end
+
+  def choose
+  
+  end
+
+end%  
